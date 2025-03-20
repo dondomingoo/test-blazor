56775d7a6e58adb69bab0ebf19d4f2db4b4c72ae
markus <modj62361@edu.ucl.dk>
2025-03-20 01:15:44 +0100

    persistant char

---
+++ Shared/ChatMessageComparer.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp.Shared;
+using System.Collections.Generic;
+using System;
+
+public class ChatMessageComparer : IEqualityComparer<ChatMessage>
+{
+    public bool Equals(ChatMessage x, ChatMessage y)
+    {
+        return x?.User == y?.User && x?.Message == y?.Message && x?.Timestamp == y?.Timestamp;
+    }
+
+    public int GetHashCode(ChatMessage obj)
+    {
+        return (obj.User?.GetHashCode() ?? 0)
+             ^ (obj.Message?.GetHashCode() ?? 0)
+             ^ obj.Timestamp.GetHashCode();
+    }
+}
